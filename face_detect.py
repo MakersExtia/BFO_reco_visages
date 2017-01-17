6142029b062511b880871f7ed43f866b3a730488
CM Makers d'Extia <makersextia@gmail.com>
2017-01-17 14:04:12 +0100

    Update face_detect.py

--- face_detect.py
+++ face_detect.py
@@ -2,8 +2,8 @@ import cv2
 import sys
 
 # Get user supplied values
-imagePath = sys.argv[1]
-cascPath = sys.argv[2]
+imagePath = '/home/pi/Desktop/img.jpg'
+cascPath = sys.argv[1]
 
 # Create the haar cascade
 faceCascade = cv2.CascadeClassifier(cascPath)
